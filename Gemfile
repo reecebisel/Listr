dc73a355cf926f0a615b22f885c11ca5dd5c7ef5
Brianne Duffy <duffymbrianne@gmail.com>
2018-12-17 08:06:12 -0700

    updated gemfile and app.rb for testing

--- Gemfile
+++ Gemfile
@@ -14,7 +14,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'slim'
 
 group :development, :test do
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'sqlite3', '1.3.13'
+  gem 'pry'
   gem 'minitest-rails'
 end
 
@@ -23,6 +24,14 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'rails-controller-testing', '1.0.2'
+  gem 'minitest',                 '5.10.3'
+  gem 'minitest-reporters',       '1.1.14'
+  gem 'guard',                    '2.13.0'
+  gem 'guard-minitest',           '2.4.4'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,6 @@
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-# require "action_mailer/railtie"
-require "action_view/railtie"
-# require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -28,6 +17,6 @@ module Listr
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
   end
 end
