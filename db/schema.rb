469efdd2fdaa7a91780d99e640467f2928b2fc1d
Brianne Duffy <duffymbrianne@gmail.com>
2018-12-17 07:49:22 -0700

    new list titles append to index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_233527) do
+ActiveRecord::Schema.define(version: 2018_12_07_210031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "list_items", force: :cascade do |t|
     t.bigint "list_id"
