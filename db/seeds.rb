5674c6cff28d1aca8e852b2ad4e5f8868df3899c
pcbmaster <fitnessnonsense@gmail.com>
2018-12-17 13:27:24 -0700

    fixed titles and descriptions not being shown

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require minitest
+
+puts "Making some lists..."
+List.create(title: "Favorite Games", description: "A list of all my favorite games")
+List.create(title: "Favorite Movies", description: "A list of all my favorite movies")
+List.create(title: "Today's ToDos", description: "A list of things that I need to do today")
+puts "Putting items on each list..."
+#put some shit here
+puts "Running tests..."
+runTests
+puts "Done!"
+
+def runTests()
+  
+end
+
